06776e0953612d87e71d157d333be91a2ba0469b
JeCoas <127827888+JeCoas@users.noreply.github.com>
2023-04-03 22:32:59 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MobilApp
+{
+    internal class Mobil
+    {
+        //fields
+        private string _warna;
+        private float _jumlahPintu;
+        private string _merek;
+        private string _model;
+        private float _tahunKeluaran;
+
+        // Properti mobil
+        public string Warna
+        {
+            get { return _warna; }
+            set { _warna = value; }
+        }
+
+        public float JumlahPintu
+        {
+            get { return _jumlahPintu; }
+            set { _jumlahPintu = value; }
+        }
+
+        public string Merek
+        {
+            get { return _merek; }
+            set { _merek = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public float TahunKeluaran
+        {
+            get { return _tahunKeluaran; }
+            set { _tahunKeluaran = value; }
+        }
+
+        // Method mobil
+        public void HitungKecepatan(int jarak, int waktu)
+        {
+            double kecepatan = (double)jarak / waktu;
+            Console.WriteLine($"Kecepatan mobil adalah {kecepatan} km/jam");
+        }
+
+        public void Klakson()
+        {
+            Console.WriteLine("Bunyi Klakson    : Pimp! Pimp!");
+        }
+
+        public void TampilInfo()
+        {
+            Console.WriteLine($"Merek Mobil      : {Merek}");
+            Console.WriteLine($"Dengan model     : {Model}");
+            Console.WriteLine($"Variasi warna    : {Warna}");
+            Console.WriteLine($"Jumlah Pintu     : {JumlahPintu} pintu");
+            Console.WriteLine($"Keluaran tahun   : {TahunKeluaran}");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MobilApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil = new Mobil();
+
+            // Set properti mobil
+            mobil.Warna = "Putih";
+            mobil.JumlahPintu = 4;
+            mobil.Merek = "Kijang";
+            mobil.Model = "Innova";
+            mobil.TahunKeluaran = 2015;
+
+            // Panggil method mobil
+            mobil.TampilInfo();
+            mobil.Klakson();
+            mobil.HitungKecepatan(160, 2);
+
+            Console.ReadKey();
+        }
+    }
+}
+
